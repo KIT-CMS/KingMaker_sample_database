3fea525360da9679dcdda4b932d197e654f1343b
Olha Lavoryk <olhalavoryk@gmail.com>
2024-02-19 13:53:24 +0100

    One can calculate genweights from files that are not in DAS

--- calculate_genweights.py
+++ calculate_genweights.py
@@ -5,6 +5,8 @@ import time
 import uproot
 from rich.progress import Progress
 import numpy as np
+import os
+import yaml
 
 
 def read_filelist_from_das(dbs):
@@ -84,3 +86,48 @@ def calculate_genweight_uproot(dataset):
         genweight = 1 - 2 * negfrac
         print(f"Final genweight: {genweight}")
         return genweight
+    
+def calculate_genweight_local_files():
+    loc_file = "sample_database/local_config.yaml"
+
+    if os.path.isfile(loc_file) and os.path.getsize(loc_file) > 0:  
+        print("it exists")
+        with open(loc_file, "r") as f:
+            local_config = yaml.safe_load(f)  
+            filelist = local_config["filelist"]
+            # print(filelist)
+            negative = 0
+            positive = 0
+            # set a threshold that if more than 10% of the files fail, the function returns None
+            threshold = len(filelist) // 10
+            fails = 0
+
+            print(f"Threshold for failed files: {threshold}")
+            print(f"Number of files: {len(filelist)}")
+            # loop over all files and count the number of negative and positive genweights
+            with Progress() as progress:
+                task = progress.add_task("Files read ", total=len(filelist))
+                filelist = [file + ":Events" for file in filelist]
+                for i, file in enumerate(filelist):
+                    try:
+                        events = uproot.open(file, timeout=5)
+                        array = events["genWeight"].array(library="np")
+                        negative += np.count_nonzero(array < 0)
+                        positive += np.count_nonzero(array >= 0)
+                        # print(f"File {i+1}/{len(filelist)} of {dataset['nick']} read")
+                        progress.update(task, advance=1)
+                    except Exception as e:
+                        print("Error when reading input file")
+                        print(e)
+                        fails += 1
+                    if fails > threshold:
+                        print("Too many files failed, returning None")
+                        return None
+                print(f"Negative: {negative} // Positive: {positive}")
+                negfrac = negative / (negative + positive)
+                genweight = 1 - 2 * negfrac
+                print(f"Final genweight: {genweight}")
+                return genweight
+    else:
+        print("No local_config.yaml found, your weight is 0.0")
+        return 0.0
